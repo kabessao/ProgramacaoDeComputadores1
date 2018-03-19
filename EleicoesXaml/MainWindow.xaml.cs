2345c51a86e51d94027ab707b775c3c4b56d7ea3
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-03-19 20:42:02 -0300

    Desenvolvendo o sistema

---
+++ EleicoesXaml/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EleicoesXaml
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int VotosJc = 0;
+        int VotosPicaPau = 0;
+        int VotosPivete = 0;
+        int VotosLinus = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnVotarJc.Click += (s, e) => VotosJc++;
+            btnVotarPicaPau.Click += (s, e) => VotosPicaPau++;
+            btnVotarLinus.Click += (s, e) => VotosLinus++;
+            btnVotarPiv.Click += (s, e) => VotosPivete++;
+        }
+
+        private void Resetar(object sender, RoutedEventArgs e)
+        {
+            VotosJc = 0;
+            VotosLinus = 0;
+            VotosPivete = 0;
+            VotosPivete = 0;
+        }
+
+        private void Apurar(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
