bd8c94ebf10e0abc5c887ddaa136da24992eaf5a
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-04-01 21:16:53 -0300

    QuizXaml em estado funcional

--- QuizXaml/MainWindow.xaml.cs
+++ QuizXaml/MainWindow.xaml.cs
@@ -20,9 +20,93 @@ namespace QuizXaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Button btnIniciar = new Button();
+        List<Pergunta> Perguntas = Pergunta.GerarPerguntas();
+        short Numero = 0;
+        public short Acertos
+        {
+            get
+            {
+                return short.Parse(txbAcertos.Text);
+            }
+            set
+            {
+                txbAcertos.Text = $"{value}";
+            }
+        }
+        public short Erros
+        {
+            get
+            {
+                return short.Parse(txbErros.Text);
+            }
+            set
+            {
+                txbErros.Text = $"{value}";
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            btnIniciar.Click += Inicio;
+            btnIniciar.Content = new TextBlock().Text = "Iniciar";
+            Painel.Children.Add(btnIniciar);
+        }
+
+        private void Inicio(object sender, RoutedEventArgs e)
+        {
+            Acertos = 0;
+            Erros = 0;
+            btnVerdadeiro.IsEnabled = true;
+            btnFalso.IsEnabled = true;
+            Painel.Children.Remove(btnIniciar);
+            Numero = 0;
+            ProximaPergunta();
+        }
+
+        private void ProximaPergunta()
+        {
+            if (Numero != Perguntas.Count)
+            {
+                txbPergunta.Text = Perguntas[Numero].Texto;
+            } else
+            {
+                MessageBox.Show("O jogo Acabou");
+                MostrarResultado();
+                txbPergunta.Text = "[Perguntas]";
+                btnFalso.IsEnabled = false;
+                btnVerdadeiro.IsEnabled = false;
+                Painel.Children.Add(btnIniciar); 
+            }
+        }
+
+        private void MostrarResultado()
+        {
+            string mensagem = "";
+            string ace = "nada, que bosta em!";
+            if (Acertos != 0) ace = $"{Acertos} perguntas";
+            if (Acertos > (Perguntas.Count / 2))
+                mensagem = "Parabens";
+            else
+                mensagem = "Que decepção";
+            MessageBox.Show(mensagem + ", você acertou " + ace);
+        }
+
+        private void Resposta(object sender, RoutedEventArgs e)
+        {
+            bool resposta = ((sender as Button).Content.ToString() == "Verdadeiro");
+            if (Numero != Perguntas.Count)
+            if (resposta == Perguntas[Numero].Resposta)
+            {
+                Acertos++; 
+            }            
+            else
+            {
+                Erros++;
+            }
+            Numero++;
+            ProximaPergunta();
         }
     }
 }
---
+++ QuizXaml/Pergunta.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace QuizXaml
+{
+    public class Pergunta
+    {
+        public string Texto { get; set; }
+        public bool Resposta { get; set; }
+        public static List<Pergunta> GerarPerguntas()
+        {
+            return new List<Pergunta>()
+            {
+                new Pergunta()
+                {
+                    Texto = "O nome do criador do kernel Linux é\nLunus Trovalds!",
+                    Resposta = false
+                },
+                new Pergunta()
+                {
+                    Texto = "Existem varios tipos de G.P.L!",
+                    Resposta = false
+                },
+                new Pergunta()
+                {
+                    Texto = "O sistema Operacional Windows atualmente\ndomina o mercado" ,
+                    Resposta = true
+                },
+                new Pergunta(){
+                    Texto = "O significado da vida, do universo\ne tudo mais é 42",
+                    Resposta = true
+                },
+                new Pergunta()
+                {
+                    Texto = "Hardware são aplicativos e\nSoftware é a parte fisica",
+                    Resposta = false
+                }
+
+            };
+        }
+    } 
+}
