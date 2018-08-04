dcf26e07f6f3dedbdc957995b58f22d1da348348
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-08-04 05:45:00 -0300

    Ultimo commit

--- ZooXaml/MainWindow.xaml.cs
+++ ZooXaml/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataAccess;
 
 namespace ZooXaml
 {
@@ -20,9 +21,109 @@ namespace ZooXaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Animal _animal = new Animal();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            GerarLista();
+
+            btnPesquisar.Click += (s, e) => GerarLista();
+
+            txtPesquisa.KeyUp += (s, e) => GerarLista();
+
+            btnSalvar.Click += (s, e) => Salvar();
+
+            btnCancelar.Click += (s, e) => Cancelar();
+
+            btnDeletar.Click += (s, e) => Deletar();
+
+            Lista.MouseDoubleClick += (s, e) => SelectLista();
+
+            DataContext = _animal;            
+
+        }
+
+        public void SelectLista()
+        {
+            if (Lista.SelectedIndex > -1)
+            {
+                Animal a = (Lista.SelectedItem as Animal);
+                _animal.Nome = a.Nome;
+                _animal.Id = a.Id;
+                _animal.Descricao = a.Descricao;
+                btnSalvar.Content = new TextBlock().Text = "Atualizar";
+            }
+            else
+            {
+                MessageBox.Show("Nenhum item selecionado");
+            }
+            
+        }
+
+        public void Cancelar()
+        {
+            _animal.Nome = "";
+            _animal.Descricao = "";
+            _animal.Id = 0;
+            btnSalvar.Content = new TextBlock().Text = "Salvar";
+        }
+        
+        public void GerarLista()
+        {
+            List<Animal> list = new List<Animal>();
+            try
+            {
+                list = new AnimalDao().Select(txtPesquisa.Text);
+            }
+            finally { }
+
+            Lista.ItemsSource = list;
+
+        }
+
+        private void Salvar()
+        {
+
+            if (btnSalvar.Content.ToString() == "Salvar")
+            {
+                if (!string.IsNullOrEmpty(_animal.Nome) || !string.IsNullOrEmpty(_animal.Descricao))
+                {
+                    new AnimalDao().Insert(_animal);
+                    Cancelar();
+                    GerarLista();
+                    MessageBox.Show("Salvo com sucesso", "Sucesso");
+
+                }
+                else
+                {
+                    MessageBox.Show("Campos em branco");
+                }
+            }
+            else if (btnSalvar.Content.ToString() == "Atualizar")
+            {
+                new AnimalDao().Update(_animal);
+                MessageBox.Show("Atualizado com sucesso", "Sucesso");
+                Cancelar();
+                GerarLista();
+            }
+        }
+
+        public void Deletar()
+        {
+            Animal animal;
+            if (Lista.SelectedIndex > -1)
+            {
+                animal = (Lista.SelectedItem as Animal);
+                if (MessageBox.Show($"Tem certeza que deseja deletar {animal.Nome}?", "Confirmação", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                    new AnimalDao().Delete(animal.Id);
+                GerarLista();
+            }
+            else
+            {
+                MessageBox.Show("nenhum item selecionado");
+            }
         }
     }
 }
